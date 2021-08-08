0fa8c68e9cb65e08c6f0b52bb1124a0b4b1865d3
LongYun <LongYun@MIBOOK>
2021-08-08 15:00:56 +0800

    添加项目文件。

---
+++ 远程桌面记录删除助手/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace 远程桌面记录删除助手
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.refresh = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.selectall = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.select = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.子项 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.item_sort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lab_github = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(33, 12);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(100, 40);
+            this.refresh.TabIndex = 0;
+            this.refresh.Text = "刷新列表";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(161, 12);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(100, 40);
+            this.delete.TabIndex = 1;
+            this.delete.Text = "确认删除";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.selectall);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(399, 412);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "查询到的记录";
+            // 
+            // selectall
+            // 
+            this.selectall.AutoSize = true;
+            this.selectall.Location = new System.Drawing.Point(7, 389);
+            this.selectall.Name = "selectall";
+            this.selectall.Size = new System.Drawing.Size(59, 19);
+            this.selectall.TabIndex = 1;
+            this.selectall.Text = "全选";
+            this.selectall.UseVisualStyleBackColor = true;
+            this.selectall.CheckedChanged += new System.EventHandler(this.select_all_CheckedChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.select,
+            this.子项,
+            this.Address});
+            this.dataGridView1.Location = new System.Drawing.Point(6, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(387, 362);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // select
+            // 
+            this.select.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.select.FillWeight = 15F;
+            this.select.HeaderText = "";
+            this.select.MinimumWidth = 6;
+            this.select.Name = "select";
+            this.select.ReadOnly = true;
+            // 
+            // 子项
+            // 
+            this.子项.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.子项.FillWeight = 30F;
+            this.子项.HeaderText = "项";
+            this.子项.MinimumWidth = 6;
+            this.子项.Name = "子项";
+            this.子项.ReadOnly = true;
+            // 
+            // Address
+            // 
+            this.Address.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Address.HeaderText = "Address";
+            this.Address.MinimumWidth = 6;
+            this.Address.Name = "Address";
+            this.Address.ReadOnly = true;
+            this.Address.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Address.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // item_sort
+            // 
+            this.item_sort.Location = new System.Drawing.Point(287, 12);
+            this.item_sort.Name = "item_sort";
+            this.item_sort.Size = new System.Drawing.Size(100, 40);
+            this.item_sort.TabIndex = 3;
+            this.item_sort.Text = "项重新排序";
+            this.item_sort.UseVisualStyleBackColor = true;
+            this.item_sort.Click += new System.EventHandler(this.item_sort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 490);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "开源地址：";
+            // 
+            // lab_github
+            // 
+            this.lab_github.AutoSize = true;
+            this.lab_github.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lab_github.Location = new System.Drawing.Point(89, 490);
+            this.lab_github.Name = "lab_github";
+            this.lab_github.Size = new System.Drawing.Size(55, 15);
+            this.lab_github.TabIndex = 5;
+            this.lab_github.Text = "GitHub";
+            this.lab_github.Click += new System.EventHandler(this.lab_github_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 514);
+            this.Controls.Add(this.lab_github);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.item_sort);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.refresh);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(441, 561);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "远程桌面记录删除助手";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox selectall;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn select;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 子项;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.Button item_sort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lab_github;
+    }
+}
+
---
+++ 远程桌面记录删除助手/Form1.cs
@@ -0,0 +1,265 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 远程桌面记录删除助手
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = this.Text + " V1.1";
+            lab_github.ForeColor = SystemColors.Highlight;
+            refresh.Left = (this.Width - refresh.Width -delete.Width - item_sort.Width-20*2)/2-8;
+            delete.Left = refresh.Right + 20;
+            item_sort.Left = delete.Right + 20;
+            mstsc_info();
+        }
+
+
+        //从注册表获取信息
+        public void mstsc_info()
+        {
+            dataGridView1.Rows.Clear();
+            try
+            {
+                string[] info;
+                RegistryKey Key;
+                Key = Registry.CurrentUser;
+                RegistryKey myreg = Key.OpenSubKey("SOFTWARE\\Microsoft\\Terminal Server Client\\Default");
+                info = myreg.GetValueNames();
+                myreg.Close();
+                for (int i = 0; i < info.Count(); i++)
+                {
+                    //获取键值
+                    string zhi = "";
+                    RegistryKey Keyzhi;
+                    Keyzhi = Registry.CurrentUser;
+                    RegistryKey kzhi = Key.OpenSubKey("SOFTWARE\\Microsoft\\Terminal Server Client\\Default");
+                    zhi = kzhi.GetValue(info[i]).ToString();
+                    kzhi.Close();
+                    DataGridViewRow dgvr = new DataGridViewRow();
+                    foreach (DataGridViewColumn c in dataGridView1.Columns)
+                    {
+                        dgvr.Cells.Add(c.CellTemplate.Clone() as DataGridViewCell);
+                    }
+                    dgvr.Cells[1].Value = info[i];
+                    dgvr.Cells[2].Value = zhi;
+                    dataGridView1.Rows.Add(dgvr);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("获取信息失败。\r\n错误原因："+ex.ToString());
+            }
+            
+            
+        }
+        public int delay;
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            refresh.Enabled = false;
+            refresh.Text = "刷新成功 3";
+            while (this.dataGridView1.Rows.Count != 0)
+            {
+                this.dataGridView1.Rows.RemoveAt(0);
+            }
+            mstsc_info();
+            //没啥用的刷新后的等待
+            delay=3;
+            timer1.Enabled=true;
+        }
+        //全选按钮
+        private void select_all_CheckedChanged(object sender, EventArgs e)
+        {
+            if (selectall.Checked == true)
+            {
+                //获取列表的行数
+                int sum;
+                sum = dataGridView1.Rows.Count;
+                for (int i = 0; i < sum; i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = "true";
+                }
+            }
+            else
+            {
+                int sum;
+                sum = dataGridView1.Rows.Count;
+                for (int i = 0; i < sum; i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = "false";
+                }
+            }
+        }
+        //单选
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                if (dataGridView1.Columns[e.ColumnIndex].Name.ToString() == "select")
+                {
+                    if (dataGridView1.Rows[e.RowIndex].Cells[0].Value != "true")
+                        dataGridView1.Rows[e.RowIndex].Cells[0].Value = "true";
+                    else
+                        dataGridView1.Rows[e.RowIndex].Cells[0].Value = "false";
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            delay--;
+            if(delay>0)
+            {                
+                refresh.Text = "刷新成功 " + delay.ToString();
+            }
+            else
+            {
+                refresh.Text = "刷新列表";
+                refresh.Enabled = true;
+                timer1.Enabled = false;
+            }
+            
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            int sum;bool hs=false;
+            sum = dataGridView1.Rows.Count;
+            for (int i = 0; i < sum; i++)
+            {
+                //判断是否勾选
+                if (dataGridView1.Rows[i].Cells[0].Value == "true")
+                {
+                    hs = true;
+                }
+            }
+            if (hs == false) { MessageBox.Show("请选择需要删除的记录。");return; }
+            //显示警告
+            DialogResult result = MessageBox.Show("此删除操作后，不可恢复，请确认是否删除","警告",MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            //再次判断勾选，删除勾选的
+            if (result == DialogResult.OK)
+            {
+                //删除记录                
+                for (int i = 0; i < sum; i++)
+                {
+                    string xiang="";
+                    //判断是否勾选
+                    if (dataGridView1.Rows[i].Cells[0].Value == "true")
+                    {
+                        xiang = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                        try
+                        {
+                            RegistryKey delKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Terminal Server Client\\Default", true);
+                            delKey.DeleteValue(xiang);                           
+                            delKey.Close();
+                        }catch(Exception ex)
+                        {
+                            MessageBox.Show("删除失败。\r\n错误原因："+ex.ToString());
+                            return;
+                        }                                               
+                    }
+                }
+                MessageBox.Show("删除成功");
+                while (this.dataGridView1.Rows.Count != 0)
+                {
+                    this.dataGridView1.Rows.RemoveAt(0);
+                }
+                mstsc_info();
+            }
+            else
+            {
+                MessageBox.Show("取消删除");
+            }
+            if (sort() == true)
+            {
+                mstsc_info();
+                MessageBox.Show("重新排序成功");
+            }
+            else
+            {
+                MessageBox.Show("重新排序error");
+            }
+
+
+        }
+
+        private void item_sort_Click(object sender, EventArgs e)
+        {
+            if (sort() == true)
+            {
+                mstsc_info();
+                MessageBox.Show("重新排序成功");
+            }
+            else
+            {
+                MessageBox.Show("重新排序error");
+            }
+        }
+
+        private bool sort()
+        {
+            bool res=false;
+            try
+            {
+                //获取项名
+                string[] info;
+                RegistryKey Key;
+                Key = Registry.CurrentUser;
+                RegistryKey myreg = Key.OpenSubKey("SOFTWARE\\Microsoft\\Terminal Server Client\\Default");
+                info = myreg.GetValueNames();
+                myreg.Close();
+                int mstsc_sum = info.Length;
+                List<string> zhi = new List<string>();
+                zhi.Clear();
+                //获取键值，用法找不到重命名键名的，所以和值一起操作
+                for (int j = 0; j < mstsc_sum; j++)
+                {
+                    //获取键值               
+                    RegistryKey Keyzhi;
+                    Keyzhi = Registry.CurrentUser;
+                    RegistryKey kzhi = Key.OpenSubKey("SOFTWARE\\Microsoft\\Terminal Server Client\\Default");
+                    zhi.Add(kzhi.GetValue(info[j]).ToString());
+                    kzhi.Close();
+                }
+                //重新排序
+                //微软远程桌面最多保留10个记录，每次连接后，所有记录都会往后挪一位，处于第10的位置往后挪就删掉了
+                for (int i = 0; i < mstsc_sum; i++)
+                {
+                    //先删除这个项，然后从第一个开始添加
+                    RegistryKey delKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Terminal Server Client\\Default", true);
+                    delKey.DeleteValue(info[i]);
+                    delKey.Close();
+                    RegistryKey key = Registry.CurrentUser;
+                    RegistryKey software = key.OpenSubKey("SOFTWARE\\Microsoft\\Terminal Server Client\\Default", true); //该项必须已存在
+                    software.SetValue("MRU" + i.ToString(), zhi[i]);
+                }
+                res = true;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("获取信息失败。\r\n错误原因：" + ex.ToString());
+            }
+                       
+            return res;
+        }
+
+        private void lab_github_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/dreamstation625/Remote-desktop-record-deletion-assistant");
+        }
+    }
+}
